814bad2447f495a77cd4d4e4030acff6dbe6e88f
harish4747 <harich4747@gmail.com>
2024-12-30 23:34:59 +0530

    chore: format code with Prettier

--- userApi.js
+++ userApi.js
@@ -8,47 +8,41 @@ parentdiv.setAttribute("id", "parentdiv");
 body.append(parentdiv);
 
 function showDetails(userdetails) {
+  userdetails.map((details) => {
+    let innerdiv = document.createElement("div");
+    innerdiv.classList.add("innerdiv");
 
-    userdetails.map((details) => {
-        let innerdiv = document.createElement("div");
-        innerdiv.classList.add("innerdiv");
+    let avatar = document.createElement("img");
+    avatar.classList.add("images");
+    avatar.src = details.avatar_url;
+    avatar.alt = "image not found";
 
-        let avatar = document.createElement("img");
-        avatar.classList.add("images");
-        avatar.src = details.avatar_url;
-        avatar.alt = "image not found"
+    let userid = document.createElement("p");
+    userid.innerHTML = "<b>UserId:</b> " + details.id;
 
-        let userid = document.createElement("p");
-        userid.innerHTML = "<b>UserId:</b> " + details.id;
+    let uname = document.createElement("p");
+    uname.innerHTML = "<b>User Name: </b>" + details.login;
 
+    let button = document.createElement("BUTTON");
+    button.innerText = "More Details";
 
-        let uname = document.createElement("p");
-        uname.innerHTML = "<b>User Name: </b>" + details.login;
+    innerdiv.append(avatar);
+    innerdiv.append(userid);
+    innerdiv.append(uname);
+    innerdiv.append(button);
 
-        let button = document.createElement("BUTTON");
-        button.innerText = "More Details";
-
-        innerdiv.append(avatar);
-        innerdiv.append(userid);
-        innerdiv.append(uname);
-        innerdiv.append(button);
-
-        parentdiv.append(innerdiv);
-
-
-    })
+    parentdiv.append(innerdiv);
+  });
 }
 
-
 async function userApi(url) {
-    try {
-        let userdetails = await (await fetch(url)).json();
-        console.log(userdetails);
-        showDetails(userdetails);
-    } catch (error) {
-        console.log(error);
-
-    }
+  try {
+    let userdetails = await (await fetch(url)).json();
+    console.log(userdetails);
+    showDetails(userdetails);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 userApi(url);
